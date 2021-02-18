630a73943fe8a6ad7d25fcb33b207fcdca3059bd
Marques <MBatoon1996@gmail.com>
2021-02-18 01:32:33 -1000

    sidebar component created with bootstrap classes

--- src/script.js
+++ src/script.js
@@ -3,3 +3,13 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light"></nav>
     );
 }
+
+const Sidebar = () => {
+    return (
+        <div className="d-none d-md-block col-md-3">
+            <div className="border border-primary py-4 px-3">
+                Sidebar
+            </div>
+        </div>
+    )
+}
